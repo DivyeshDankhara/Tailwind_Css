e6201c103b29b4c43d633e4431b091e97e5ada27
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-07-19 11:20:10 +0530

    19-07-2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {  
     extend: {
+      colors: {
+        yellow: '#ffb400',
+        gray: '#666666',
+        backg: '#111111'
+      },
+
       fontFamily: {
-        'abc': ['Oswald'],
       }
     },
   },
